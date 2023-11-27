7259ad0e1b0a86012b7f6d290df6946d939b7263
Jasper Funk-smit <^_^@arbitrarysystems.com>
2023-11-27 21:30:23 +0100

    minor

--- html/arbitrarysystems/modules/app/gour/module.js
+++ html/arbitrarysystems/modules/app/gour/module.js
@@ -13,12 +13,19 @@ Module.export({requires:[
         var node = document.createElement('a');
         node.style.width = '100%';
         node.style.height = '100%';
-        node.style.display = 'block'
+        node.style.display = 'block';
+        node.style.color = 'white';
+        node.style.padding = '1em';
         node.style.backgroundImage = `url(/modules/app/gour/preview.png)`
+        node.style.backgroundPosition = 'center'
 
-        node.target = '_blank'
+        //node.target = '_blank'
         node.href = '/modules/app/gour/index.html'
-
+        node.innerHTML = `
+            The royal game of Ur<br/>
+            <a style="color:#ccc;font-size:0.8em;" target="_blank" href="https://en.wikipedia.org/wiki/Royal_Game_of_Ur">rules on wikipedia</a>
+            <div style="margin-top:2em; font-size:0.8em; color:#ccc;">click to play</div>
+        `
        
 
         var target = options.target || document.getElementsByTagName('body')[0];
